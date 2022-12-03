54d52f28dfc300a2d7d7fd2907aab9f695cb01d5
Yunus Alıcı <yunusac51@gmail.com>
2022-12-03 04:37:03 +0300

    first commit and bootstrap adding.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <button type="button" className="btn btn-primary">
+      Bootstrap Adding.
+    </button>
+  );
+}
+
+export default App;
